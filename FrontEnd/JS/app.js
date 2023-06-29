9f897820e60d9686865d4e5ac88c50bc25160167
loetitia <loetitia59@gmail.com>
2023-06-29 06:53:59 +0200

    création d'une ligne de filtres par catégories

---
+++ FrontEnd/JS/app.js
@@ -0,0 +1,29 @@
+//récupérer des données de l'API "works"
+fetch ('http://localhost:5678/api/works')
+.then((works) => works.json())
+.then((data) => console.log(data));
+
+//récupérer des données de l'API "catégories"
+fetch ('http://localhost:5678/api/categories')
+.then((categories) => categories.json())
+.then((data) => console.log(data));
+
+const works = async() => {
+   const reponse = await fetch ('http://localhost:5678/api/works');
+   const data = await reponse.json();
+}
+
+const categories = async() => {
+    const reponse = await fetch ('http://localhost:5678/api/categories');
+    const data = await reponse.json();
+ }
+
+//création d'une liste de filtres en triant les catégories 
+const filterList = Array.from(new set(works.map(work =>work.categoryId)));
+console.log(filterList);
+
+function addElement() {
+    const button = document.createElement()
+}
+const gallery = document.querySelector('.gallery');
+ gallery.innerHTML = " ";
--- FrontEnd/assets/app.js
+++
@@ -1,3 +0,0 @@
-fetch ('http://localhost:5678/api/works')
-.then((response) => response.json())
-.then((data) => console.log(data));
